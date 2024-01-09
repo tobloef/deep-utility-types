f9389f7038cceb87fe8e9309da8443711ea330e7
Tobias Løfgren <tobloef@gmail.com>
2024-01-09 16:47:58 +0100

    feat: expose `KeysAsDotNotation` and `DefaultIgnoredTypes`

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './deep-types/deep-omit';
 export * from './deep-types/deep-pick';
 export * from './deep-types/deep-optional';
 export * from './deep-types/deep-require';
+export * from './util-types/keys-as-dot-notation';
